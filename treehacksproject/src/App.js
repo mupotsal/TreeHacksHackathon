94180e913d6e50d560a85378cfb208cf4b43633f
alex <aogalkin17@gmail.com>
2021-02-13 14:15:50 -0800

    Improved Login and Settings Pages

--- treehacksproject/src/App.js
+++ treehacksproject/src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './Login.css';
 import Settings from './Settings.js'
-
+import Login from './Login.js'
 
 
 class App extends Component {
@@ -23,34 +23,12 @@ class App extends Component {
     return (
 
       <html>
-        <title>Boogaloo Login</title>
-          <body class="LoginBackground">
+        <Login>
+        </Login>
+        <Settings>
+        </Settings>
 
 
-
-            <div class="WelcomeFormat">
-              <h1>Welcome to Boogaloo!</h1>
-              <i>Keeping commutities connected</i>
-            </div>
-
-            <div class="Login">
-              <form>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <label class="box" for="ID">ID:</label>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <input type="text"></input>
-                <br></br>
-                <label class="box" for="Password:">Password:</label>
-                <input type="text"></input>
-                <br></br>
-                <button class="but" type="button" name="LoginButton">Login</button>
-
-              </form>
-
-            </div>
-            <Settings>
-            </Settings>
-          </body>
       </html>
 
     );
--- treehacksproject/src/Settings.js
+++ treehacksproject/src/Settings.js
@@ -11,15 +11,31 @@ class Settings extends Component {
     return (
       <html>
         <title>Settings</title>
-          <body class="SettingsBackground">
+          <body className="AboutMeInfo">
 
-            <div class="AboutMe">
-              <h3>About Me</h3>
-                <img src="ProfilePicture.png"></img>
+            <div>
+              <h2>About Me</h2>
               <br></br>
-              <input class="AboutMeInfo" type="text"></input>
-            </div>
+            <div>
 
+                <img className="ProfilePicture" src={require("./Images/ProfilePicture.png").default} height={200} width={200}></img>
+              </div>
+              <br></br>
+                <input className="Info" type="text"></input>
+              <br></br>
+              <br></br>
+              <button className="Submit" type="button" name="SubmitButton">Submit</button>
+              <br></br>
+
+              <div>
+                <br></br>
+                <button onclick="MainPage.js" className="NavigationButtons" type="button" name="MainPageButton"><a href="MainPage.js">Main Page</a></button>
+                <br></br>
+                <br></br>
+                <button onclick="Login.js" className="NavigationButtons" type="button" name="Logout"><a href="Login.js">Logout</a></button>
+              </div>
+
+            </div>
           </body>
       </html>
 
