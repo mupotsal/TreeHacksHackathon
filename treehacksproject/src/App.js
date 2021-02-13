15af8866b95fc73bf8c2f4ecfcbd1d7a4bbf3a12
alex <aogalkin17@gmail.com>
2021-02-12 21:56:27 -0800

    Login page first version

--- treehacksproject/src/App.js
+++ treehacksproject/src/App.js
@@ -3,23 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <html>
+    <title>Boogaloo Login</title>
+      <body>
+
+
+
+        <div class="WelcomeFormat">
+          <h1>Welcome to Boogaloo!</h1>
+          <i>Keeping commutities connected</i>
+        </div>
+
+        <div class="Login">
+          <form>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <label class="box" for="ID">ID:</label>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <input type="text"></input>
+            <br></br>
+            <label class="box" for="Password:">Password:</label>
+            <input type="text"></input>
+            <br></br>
+            <button class="but" type="button" name="LoginButton">Login</button>
+
+          </form>
+
+        </div>
+
+      </body>
+  </html>
+);
+
+};
 
 export default App;
