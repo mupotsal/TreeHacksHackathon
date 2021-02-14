1a6e8eee9a20554607585a1326ea074ab8516c83
liberty Mupotsa <mupotsal@berea.edu>
2021-02-13 23:18:34 -0500

    change the padding

---
+++ treehacksproject/src/firebase 2.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyD-l95rGx_4KTY4cAgfd3ILkpTap2q9tho",
+  authDomain: "treehacksproject-f586f.firebaseapp.com",
+  databaseURL: "https://treehacksproject-f586f-default-rtdb.firebaseio.com",
+  projectId: "treehacksproject-f586f",
+  storageBucket: "treehacksproject-f586f.appspot.com",
+  messagingSenderId: "658140414882",
+  appId: "1:658140414882:web:2ebea7d9ed54bfeb91a8fb",
+  measurementId: "G-VH9S0E5MHL"
+};
+// Initialize Firebase
+
+export default firebaseConfig;
